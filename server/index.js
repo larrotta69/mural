2b9c953bcd0d772f809b898e1078b46fe32f78ca
larrotta69 <larrotta69@gmail.com>
2019-08-18 17:06:58 +0200

    setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Node app is running on port', PORT);
+});
