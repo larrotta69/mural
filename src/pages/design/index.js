11b2a92928609e86b8d0463f0cf4a03a68e97178
larrotta69 <larrotta69@gmail.com>
2019-09-19 14:49:50 +0200

    updates

--- src/pages/design/index.js
+++ src/pages/design/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'styled-components/macro';
-import { Layout, Form, Select, Input, Row, Col } from 'antd';
+import { Layout, Form, Select, Input, Row, Col, Button, Alert, Icon } from 'antd';
 import './styles.css';
 
 import Header from '../../components/Header';
 import Card from '../../components/Card';
 
 import profile from '../../img/profile.png';
+import block_1 from '../../img/block_1.png';
+import block_2 from '../../img/block_2.png';
 
 const { Option } = Select;
 
@@ -17,7 +19,7 @@ class Design extends React.Component {
                 <Row>
                     <Header/>
                 </Row>
-                <Row>
+                <Row className="wrapper">
                     <Col xs={10}>
                         <div className="form-content">
                             <Form>
@@ -38,13 +40,54 @@ class Design extends React.Component {
                                         <Option value="pe">17 deg</Option>
                                     </Select>
                                 </Form.Item>
+                                <Form.Item label="Select the blocks:">
+                                    <Row className="blocks">
+                                        <Col xs={11}>
+                                            <img alt="" src={block_1} />
+                                        </Col>
+                                        <Col xs={11} offset={2}>
+                                            <img alt="" src={block_2} />
+                                        </Col>
+                                        <Col xs={11}>
+                                            <img alt="" src={block_2} />
+                                        </Col>
+                                        <Col xs={11} offset={2}>
+                                            <img alt="" src={block_2} />
+                                        </Col>
+                                    </Row>
 
+                                </Form.Item>
+                                <Row>
+                                    <Col xs={12}>
+                                        <Button type="primary" icon="fund" size="large">
+                                            Calculate
+                                        </Button>
+                                    </Col>
+                                    <Col xs={12}>
+                                        <Button type="primary" size="large">
+                                            3D Model
+                                            <Icon type="right" />
+                                        </Button>
+                                    </Col>
+                                </Row>
                             </Form>
                         </div>
                     </Col>
                     <Col xs={14}>
+                        <Alert
+                            message="Success message"
+                            description="Design is stable"
+                            type="success"
+                            showIcon
+                        />
+                        {/* <Alert
+                            message="Error message"
+                            description="Design is not stable"
+                            type="error"
+                            showIcon
+                        /> */}
                         <div className="design-content">
-                            <img src={profile} />
+                            <img alt="" src={profile} />
                         </div>
                     </Col>
                 </Row>
--- src/pages/model/index.js
+++ src/pages/model/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import 'styled-components/macro';
-import { Layout } from 'antd';
+import { Layout, Row, Col } from 'antd';
 import './styles.css';
 
 import Header from '../../components/Header';
-import Card from '../../components/Card';
 
-import defaultProject from '../../img/default.png';
-import designProject from '../../img/design.png';
+import model from '../../img/model.png';
 
 class Model extends React.Component {
     render() {
         return (
-            <Layout className="home">
-                <Header/>
-                <div className="content">
-                    <Card imgSrc={defaultProject} description="Use our pre-designed projects" title="Default Project" />
-                    <Card imgSrc={designProject} description="Create your own project" title="Model your Project" />
-                </div>
+            <Layout className="model">
+                <Row>
+                    <Header/>
+                </Row>
+                <Row className="wrapper">
+                    <Col>
+                        <img alt="model" src={model} />
+                    </Col>
+                </Row>
             </Layout>
         );
     }
